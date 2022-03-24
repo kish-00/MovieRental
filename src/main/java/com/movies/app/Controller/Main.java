024153963cb99254484b2a76b6283a9f3f350083
kish-00 <59705198+kish-00@users.noreply.github.com>
2022-03-24 14:11:39 +0300

    add security config constructor parameter beans

--- src/main/java/com/movies/app/Controller/Main.java
+++ src/main/java/com/movies/app/Controller/Main.java
@@ -3,6 +3,12 @@ package com.movies.app.Controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -12,4 +18,24 @@ public class Main{
         SpringApplication.run(Main.class, args);
     }
 
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return null;
+            }
+        };
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    protected BCryptPasswordEncoder bCryptPasswordEncoder (){
+        return new BCryptPasswordEncoder(){
+        };
+    }
 }   
